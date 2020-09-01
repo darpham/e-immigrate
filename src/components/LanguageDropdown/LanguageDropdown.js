9aa948c0956b6e8c5b2a62fa721dc5009ec54cea
Prajwal Pyakurel <ppyakure@tibco.com>
2020-09-01 07:37:33 -0700

    added prettier and applied to all files

--- src/components/LanguageDropdown/LanguageDropdown.js
+++ src/components/LanguageDropdown/LanguageDropdown.js
@@ -5,30 +5,38 @@ import { languageOptions } from '../../data/LanguageContent';
 import './LanguageDropdown.css';
 
 const LanguageSelect = ({ setLanguage }) => {
-  return (
-    <div className='languageSelectContainer'>
-      <LanguagePicker className='languageIcon' height='2rem' width='2rem' />
-      <select onChange={ev => setLanguage(ev.target.value)} >
-        { 
-          languageOptions.map((lang, idx) => {
-            return (
-              <option className='languageOption' key={idx} value={lang.code} >{lang.full}</option>
-            )
-          })
-        }
-      </select>
-    </div>
-  );
+    return (
+        <div className="languageSelectContainer">
+            <LanguagePicker
+                className="languageIcon"
+                height="2rem"
+                width="2rem"
+            />
+            <select onChange={(ev) => setLanguage(ev.target.value)}>
+                {languageOptions.map((lang, idx) => {
+                    return (
+                        <option
+                            className="languageOption"
+                            key={idx}
+                            value={lang.code}
+                        >
+                            {lang.full}
+                        </option>
+                    );
+                })}
+            </select>
+        </div>
+    );
 };
 
 const LanguageDropdown = ({ language, setLanguage }) => {
-  return (
-    <div className='languageDropdown'>
-      <div className='languageSelect'>
-        <LanguageSelect setLanguage={ setLanguage } />
-      </div>
-    </div>
-  );
+    return (
+        <div className="languageDropdown">
+            <div className="languageSelect">
+                <LanguageSelect setLanguage={setLanguage} />
+            </div>
+        </div>
+    );
 };
 
 export default LanguageDropdown;
